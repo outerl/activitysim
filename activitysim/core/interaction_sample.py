4126a4c6d5cb206930c7011aa87af242241c88f1
Jeff Newman <jeff@driftless.xyz>
2026-03-09 13:50:26 -0500

    fix unbound variable in estimation mode

--- activitysim/core/interaction_sample.py
+++ activitysim/core/interaction_sample.py
@@ -574,6 +574,10 @@ def _interaction_sample(
         return choices_df
 
     if use_eet:
+
+        if estimation.manager.enabled:
+            raise ValueError("cannot use explicit error terms with estimation mode at this time")
+
         utilities = logit.validate_utils(
             state,
             utilities,
@@ -649,18 +653,40 @@ def _interaction_sample(
             chunk_sizer=chunk_sizer,
         )
 
-        chunk_sizer.log_df(trace_label, "choices_df", choices_df)
-
         if estimation.manager.enabled and sample_size > 0:
-            choices_df = _ensure_chosen_alts_in_sample(
-                alt_col_name,
-                alternatives,
-                choices_df,
-                choosers,
-                probs,
-                state,
-                trace_label,
+            # we need to ensure chosen alternative is included in the sample
+            survey_choices = estimation.manager.get_survey_destination_choices(
+                state, choosers, trace_label
             )
+            if survey_choices is not None:
+                assert (
+                    survey_choices.index == choosers.index
+                ).all(), "survey_choices and choosers must have the same index"
+                survey_choices.name = alt_col_name
+                survey_choices = survey_choices.dropna().astype(
+                    choices_df[alt_col_name].dtype
+                )
+
+                # merge all survey choices onto choices_df
+                probs_df = probs.reset_index().melt(
+                    id_vars=[choosers.index.name],
+                    var_name=alt_col_name,
+                    value_name="prob",
+                )
+                # probs are numbered 0..n-1 so we need to map back to alt ids
+                zone_map = pd.Series(alternatives.index).to_dict()
+                probs_df[alt_col_name] = probs_df[alt_col_name].map(zone_map)
+
+                survey_choices = pd.merge(
+                    survey_choices,
+                    probs_df,
+                    on=[choosers.index.name, alt_col_name],
+                    how="left",
+                )
+                survey_choices["rand"] = 0
+                survey_choices["prob"].fillna(0, inplace=True)
+                choices_df = pd.concat([choices_df, survey_choices], ignore_index=True)
+                choices_df.sort_values(by=[choosers.index.name], inplace=True)
 
         del probs
         chunk_sizer.log_df(trace_label, "probs", None)
